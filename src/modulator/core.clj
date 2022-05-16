ab5156164f6b61621d9f1cc806b80ddeb6be924d
qespr <rspointer@post.cz>
2022-05-16 23:38:55 +0200

    Přidána první půlka modulace RLL a poznámky k druhé půlce
    Možná je to teď trošku překomentované ale na moji obhajobu, pořád se to učím

--- src/modulator/core.clj
+++ src/modulator/core.clj
@@ -23,6 +23,35 @@
    "0010" "NNPNNPNN",
    "0011" "NNNNPNNN"})
 
+(defn- rll-match
+  "Zjistí který segment se nachází jako další v binárním řetězci
+  Vrací délku segmentu aby jste si mohli udělat substring"
+  ;;Musíme si ještě udržovat kopii všech klíčů pro případ
+  ;;že by poslední sada nebyla tabulková hodnota
+  ([klice bin-string] (rll-match klice klice bin-string))
+  ;;kk - klíč kopie, vždy předávat nezměněné
+  ([klice kk bin-string]
+  ;;vyzkoušet match pro první first z klicu
+  ;;pokud ne tak recur s rest klicu
+  ;;Tady MUSÍ nejdřív být check jestli klíče nejsou nulové -> stane se když už netrefíme nic, v tom případě se musí přidávat nuly
+  (cond
+    ;;pokud jsou klíče už prázdné - žádná kombinace se nenašla, musíme přidávat nuly;
+    ;;můžou se přidat max 2 nuly ale zkusíme to ignorovat a uvidíme jestli z toho bude někdy bug
+    (= klice '()) (recur kk kk (str bin-string "0"))
+    ;;Kombinace se našla - vracíme délku sady aby si volající funkce mohla ukrojit substring
+    (cstr/starts-with? bin-string (first klice)) (first klice)
+    :else (recur (rest klice) kk bin-string))))
+
+;;Todo: tohle taky nějak musí deketovat že došlo k doplnění
+;;      třeba tak že to vždy zkontroluje že segment není delší než zbývající string
+(defn rll-encode
+  "Zakóduje binární string pomocí specifikované RLL tabulky"
+  ([tabulka bin-string] (rll-encode tabulka bin-string nil))
+  ;;Tady se musí dávat bacha na to kddyž se nic nematchne
+  ([tabulka string-rest ret]))
+
+
+
 ;;Komplet pravidla
 ;;0 = PN if předtím 00
 ;;0 = NN if předtím 10
