3ea2665efeffb624c3499e4253422e846e918e35
qespr <rspointer@post.cz>
2022-05-16 15:28:26 +0200

    Přidána MFM modulace

--- src/modulator/core.clj
+++ src/modulator/core.clj
@@ -1,5 +1,6 @@
 (ns modulator.core
-  (:require [modulator.number-utils :as nu])
+  (:require [modulator.number-utils :as nu]
+            [clojure.string :as cstr])
   (:gen-class))
 
 (def RLL-1
@@ -22,29 +23,43 @@
    "0010" "NNPNNPNN",
    "0011" "NNNNPNNN"})
 
+;;Komplet pravidla
 ;;0 = PN if předtím 00
 ;;0 = NN if předtím 10
 ;;1 = NP
 (defn mfm-encode
   "Zakóduje binární text pomocí MFM modulace"
-  ([bin-text] (mfm-encode (first bin-text) "0" (rest bin-text)))
-  ([bit prev tail]))
+  ([bin-text] (mfm-encode (first bin-text) \0 (rest bin-text) nil))
+  ([bit prev tail ret]
+   (if (not= bit nil) ;;Dokud máme bity ke kontrole
+     (recur
+      (first tail) ;;Předáváme další bit na průzkum
+      bit ;;Předchozí bude současný bit
+      (rest tail) ;;Ocas bude ocas ocasu
+      (str ret
+           ;;Hlavní logika kde se rozhoduje o tom jaká modulace se použije
+           (if (= bit \1)
+             "NP"
+             (if (= prev \0)
+               "PN"
+               "NN"))))
+     ret)))
 
 (defn fm-encode
   "Zakóduje text pomocí FN modulace"
   [bin-text]
-  (clojure.string/replace
-   (clojure.string/replace bin-text #"0" "PN")
+  (cstr/replace
+   (cstr/replace bin-text #"0" "PN")
    #"1" "PP"))
-
 
 (defn -main
   "Vstupní bod, bere 1 argument - text k modulaci"
   [& args]
   (let [text-bin (nu/text-to-bin (first args))
         text-fm (fm-encode text-bin)
-        text-mfm "nil"
+        text-mfm (mfm-encode text-bin)
         text-rll1 "nil"
         text-rll2 "nil"]
   (println "Text: " (first args))
-  (println "FM: " text-fm)))
+  (println "  FM: " text-fm)
+  (println " MFM: " text-mfm)))
